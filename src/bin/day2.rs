5bbff2e176dee2ad394afae4d2825073881303ff
Alex Rudy <alexander.rudy@lendup.com>
2017-12-02 15:26:31 -0800

    Much-improved error handling

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -2,6 +2,7 @@ extern crate aoc2017;
 use aoc2017::puzzles::day2;
 use std::io;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
   
@@ -9,7 +10,10 @@ fn main() {
   // io::stdin().read_to_string(&mut spreadsheet).unwrap();
   
   let stdin = io::stdin();
-  let (checksum, divides) = day2::even_divides_and_checksum(stdin.lock().lines()).unwrap();
+  let (checksum, divides) = day2::even_divides_and_checksum(stdin.lock().lines()).unwrap_or_else(|err| {
+    eprintln!("Application Error: {}", err);
+    process::exit(1);
+  });
   
   // let checksum = day2::checksum(spreadsheet.as_bytes().lines()).unwrap();
   println!("Checksum: {}", checksum);
--- src/puzzles/day2.rs
+++ src/puzzles/day2.rs
@@ -2,15 +2,71 @@ use std::io;
 #[allow(unused_imports)]
 use std::io::prelude::*;
 
+use std::fmt;  
+use std;
+
+#[derive(Debug)]
+pub enum Error {  
+    NoItemError,
+    NoDivisorsError,
+    IoError(io::Error),
+    ParseError(std::num::ParseIntError),
+}
+
+impl fmt::Display for Error {  
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::NoItemError => f.write_str("No Items Found"),
+            Error::NoDivisorsError => f.write_str("No Items could be evenly divided"),
+            Error::IoError(ref e) => e.fmt(f),
+            Error::ParseError(ref e) => e.fmt(f),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::IoError(err)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+  fn from(err: std::num::ParseIntError) -> Error {
+      Error::ParseError(err)
+  }
+}
+
+impl std::error::Error for Error {  
+    fn description(&self) -> &str {
+        match *self {
+            Error::NoItemError => "No Items Found",
+            Error::NoDivisorsError => "No Items could be evenly divided.",
+            Error::IoError(_) => "An I/O Error Occured",
+            Error::ParseError(_) => "Integer parsing error",
+        }
+    }
+    
+    fn cause(&self) -> Option<&std::error::Error> {
+        match *self {
+            Error::IoError(ref err) => Some(err as &std::error::Error),
+            Error::ParseError(ref err) => Some(err as &std::error::Error),
+            _ => None,
+        }
+    }
+
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
 /// Checksums a single spreadsheet row.
-fn checksum_row(row: &[u32]) -> Result<u32, String> {
-  let biggest = row.iter().max().ok_or("No maximum!".to_string())?;
-  let smallest = row.iter().min().ok_or("No minimum!".to_string())?;
+fn checksum_row(row: &[u32]) -> Result<u32> {
+  let biggest = row.iter().max().ok_or(Error::NoItemError)?;
+  let smallest = row.iter().min().ok_or(Error::NoItemError)?;
   
   return Ok(biggest - smallest)
 }
 
-fn even_divides_row(row: &[u32]) -> Result<u32, String> {
+fn even_divides_row(row: &[u32]) -> Result<u32> {
   for (i, x) in row.iter().enumerate() {
     for y in row.iter().skip(i + 1) {
       if (y >= x) && (y % x == 0) {
@@ -20,64 +76,51 @@ fn even_divides_row(row: &[u32]) -> Result<u32, String> {
       }
     }
   }
-  return Err("No even divisors!".to_string())
+  return Err(Error::NoDivisorsError)
 }
 
 /// Turns a string of row numbers into a vector.
-fn parse_row(row: &String) -> Result<Vec<u32>, String> {
+fn parse_row(row: &String) -> Result<Vec<u32>> {
   let mut row_numbers : Vec<u32> = Vec::new();
   for number in row.trim().split(char::is_whitespace) {
-    match number.parse::<u32>() {
-      Ok(n) => row_numbers.push(n),
-      Err(_) => return Err(format!("Can't parse {}", number)),
-    }
+    row_numbers.push(number.parse::<u32>()?)
   }
   return Ok(row_numbers)
 }
 
-fn checksum_string_row(row: &String) -> Result<u32, String> {
+fn checksum_string_row(row: &String) -> Result<u32> {
   let row_numbers = parse_row(row)?;
   return checksum_row(&row_numbers);
 }
 
-fn even_divides_string_row(row: &String) -> Result<u32, String> {
+fn even_divides_string_row(row: &String) -> Result<u32> {
   let row_numbers = parse_row(row)?;
   return even_divides_row(&row_numbers);
 }
 
-pub fn even_divides<T: io::BufRead>(lines: io::Lines<T>) -> Result<u32, String> {
+pub fn even_divides<T: io::BufRead>(lines: io::Lines<T>) -> Result<u32> {
   let mut checksum = 0;
   for line in lines {
-    match line {
-      Ok(row) => checksum += even_divides_string_row(&row)?,
-      Err(err) => return Err(err.to_string())
-    }
+    checksum += even_divides_string_row(&(line?))?
   }
   return Ok(checksum)
 }
 
-pub fn checksum<T: io::BufRead>(lines: io::Lines<T>) -> Result<u32, String> {
+pub fn checksum<T: io::BufRead>(lines: io::Lines<T>) -> Result<u32> {
   let mut checksum = 0;
   for line in lines {
-    match line {
-      Ok(row) => checksum += checksum_string_row(&row)?,
-      Err(err) => return Err(err.to_string())
-    }
+    checksum += checksum_string_row(&(line?))?
   }
   return Ok(checksum)
 }
 
-pub fn even_divides_and_checksum<T: io::BufRead>(lines: io::Lines<T>) -> Result<(u32, u32), String> {
+pub fn even_divides_and_checksum<T: io::BufRead>(lines: io::Lines<T>) -> Result<(u32, u32)> {
   let mut checksum = 0;
   let mut divides = 0;
   for line in lines {
-    match line {
-      Ok(row) => {
-        checksum += checksum_string_row(&row)?;
-        divides += even_divides_string_row(&row)?;
-      }
-      Err(err) => return Err(err.to_string())
-    }
+    let row = line?;
+    checksum += checksum_string_row(&row)?;
+    divides += even_divides_string_row(&row)?;
   }
   return Ok((checksum, divides))
 }
@@ -91,7 +134,7 @@ mod tests {
     let spreadsheet = "5 9 2 8
 9 4 7 3
 3 8 6 5";
-    assert_eq!(even_divides(spreadsheet.as_bytes().lines()), Ok(9))
+    assert_eq!(even_divides(spreadsheet.as_bytes().lines()).unwrap(), 9)
   }
   
   #[test]
@@ -99,7 +142,7 @@ mod tests {
     let spreadsheet = "5 1 9 5
 7 5 3
 2 4 6 8";
-    assert_eq!(checksum(spreadsheet.as_bytes().lines()), Ok(18))
+    assert_eq!(checksum(spreadsheet.as_bytes().lines()).unwrap(), 18)
     
   }
   
@@ -108,31 +151,31 @@ mod tests {
     let spreadsheet = "5	1	9	5
     7	5	3
     2 4	6 8";
-    assert_eq!(checksum(spreadsheet.as_bytes().lines()), Ok(18))
+    assert_eq!(checksum(spreadsheet.as_bytes().lines()).unwrap(), 18)
   }
   
   #[test]
   fn parse_1() {
     let row = "2 4 6 8".to_string();
-    assert_eq!(parse_row(&row).expect("row"), vec![2,4,6,8]);
+    assert_eq!(parse_row(&row).unwrap(), vec![2,4,6,8]);
   }
   
   #[test]
   fn checksum_1() {
     let row = vec![5,1,9,5];
-    assert_eq!(checksum_row(&row).expect("answer"), 8)
+    assert_eq!(checksum_row(&row).unwrap(), 8)
   }
   
   #[test]
   fn checksum_2() {
     let row = vec![7,5,3];
-    assert_eq!(checksum_row(&row).expect("answer"), 4)
+    assert_eq!(checksum_row(&row).unwrap(), 4)
   }
   
   #[test]
   fn checksum_3() {
     let row = vec![2,4,6,8];
-    assert_eq!(checksum_row(&row).expect("answer"), 6)
+    assert_eq!(checksum_row(&row).unwrap(), 6)
   }
   
 }
