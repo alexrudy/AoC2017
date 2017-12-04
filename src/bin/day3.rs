7f334897027a5c5a9706353e6cc61a31d4450b46
Alex Rudy <alexander.rudy@lendup.com>
2017-12-03 18:07:50 -0800

    Day 3 solution

---
+++ src/bin/day3.rs
@@ -0,0 +1,8 @@
+extern crate aoc2017;
+use aoc2017::puzzles::day3;
+
+fn main() {
+  let mem = 361527;
+  println!("Memory {} takes {} steps.", mem, day3::memory_distance(mem));
+  println!("First value above {} is {}", mem, day3::first_large_value(mem).unwrap())
+}
