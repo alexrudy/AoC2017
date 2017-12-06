c72ca3ea2403e7163061c8ef240da2f830cd6ee8
Alex Rudy <alexander.rudy@lendup.com>
2017-12-06 10:12:25 -0800

    Improvement to day6 runner

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -2,12 +2,16 @@ extern crate aoc2017;
 use aoc2017::puzzles::day6;
 use std::io;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
   let stdin = io::stdin();
   let mut line = String::new();
   stdin.lock().read_line(&mut line).unwrap();
-  let memory : Vec<usize> = line.trim().split(char::is_whitespace).map(|x| x.parse::<usize>().unwrap()).collect();
+  let memory : Vec<usize> = line.trim().split(char::is_whitespace).map(|x| x.parse::<usize>().unwrap_or_else(| e | {
+    eprintln!("Error handling input: {:?}", e);
+    process::exit(1);
+  })).collect();
   
   println!("Memory {:?}", memory);
   let (nsteps, ncycle) = day6::reallocate_many(memory);
