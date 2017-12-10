96938d7d647f2a39cc92198b66aa21a34dc7c939
Alex Rudy <alexander.rudy@lendup.com>
2017-12-10 15:47:35 -0800

    Day 9 solution

---
+++ src/bin/day9.rs
@@ -0,0 +1,15 @@
+extern crate aoc2017;
+use aoc2017::puzzles::day9;
+use std::io;
+use std::io::prelude::*;
+
+fn main() {
+  let mut stdin = io::stdin();
+  let mut text = String::new();
+  stdin.read_to_string(&mut text).unwrap();
+  
+  let (score, garbage) = day9::procress_stream(&text);
+  println!("Score: {}", score);
+  println!("Garbage: {}", garbage);
+  
+}
