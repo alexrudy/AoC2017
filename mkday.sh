128092cc99486b26ca7c8991c9ae1d24df018ffc
Alex Rudy <alexander.rudy@lendup.com>
2017-12-16 13:37:50 -0800

    Improve the day maker

--- mkday.sh
+++ mkday.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 day=$1
+
 echo "Making day$day"
-touch "src/bin/day$day.rs"
+if [[ ! -e "src/bin/day$day.rs" ]]; then
+  cat > "src/bin/day$day.rs" <<EOF
+extern crate aoc2017;
+use aoc2017::puzzles::day$day;
+
+fn main() {
+  println!("Day $day solution")
+}
+EOF
+fi
 touch "src/puzzles/day$day.rs"
+
 grep "pub mod day$day;" src/puzzles/mod.rs
 if [[ $? -eq 0 ]]; then
   echo "Already added Day $day to mod.rs"
