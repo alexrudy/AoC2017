892ec2307e22d43c3a667ff0fad9406583423276
Alex Rudy <alexander.rudy@lendup.com>
2017-12-15 17:37:54 -0800

    Day 15 solution

---
+++ src/bin/day15.rs
@@ -0,0 +1,16 @@
+extern crate aoc2017;
+use aoc2017::puzzles::day15;
+
+fn main() {
+  let mut a = day15::Generator::new(16807, 699);
+  let mut b = day15::Generator::new(48271, 124);
+
+  println!("Part 1 Score: {}", day15::compete(&mut a, &mut b, 40e+6 as usize));
+  
+  let mut a = day15::Generator::new(16807, 699);
+  let mut b = day15::Generator::new(48271, 124);
+  a.picky(4);
+  b.picky(8);
+  println!("Part 2 Score: {}", day15::compete(&mut a, &mut b, 5e+6 as usize));
+  
+}
