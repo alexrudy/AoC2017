231eee0be7a1371e694892393f9b45c9d1de94df
Alex Rudy <alexander.rudy@lendup.com>
2017-12-23 11:57:25 -0800

    Day 18 Part 1 solution

---
+++ src/bin/day18.rs
@@ -0,0 +1,17 @@
+extern crate aoc2017;
+use aoc2017::puzzles::day18;
+use std::io;
+use std::io::Read;
+
+fn main() {
+  let mut program = String::new();
+  let stdin = io::stdin();
+  stdin.lock().read_to_string(&mut program).expect("Read failure!");
+  
+  match day18::run_program(&program).take(1).next() {
+    Some(sound) =>  println!("First sound emitted: {}", sound),
+    None => println!("No sound emitted!")
+  };
+  
+  
+}
